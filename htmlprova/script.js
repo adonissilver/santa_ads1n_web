ed2ce45f34fc12cd73fe175feb674c0116df9d23
Silver <51095182+adonissilver@users.noreply.github.com>
2026-05-14 16:57:35 -0300

    Exercicios simples de prova
    Exercicios simples de prova

---
+++ htmlprova/script.js
@@ -0,0 +1,41 @@
+function mostrarVitaminas(){
+
+    document.getElementById("principal").innerHTML = `
+
+        <h2>Vitaminas</h2>
+
+        <p>
+        O morango possui vitamina C,
+        antioxidantes e fibras.
+        </p>
+
+    `;
+}
+
+function mostrarReceita(){
+
+    document.getElementById("principal").innerHTML = `
+
+        <h2>Receita</h2>
+
+        <p>
+        Bata morango com leite
+        e faça uma vitamina deliciosa.
+        </p>
+
+    `;
+}
+
+function mostrarPlantio(){
+
+    document.getElementById("principal").innerHTML = `
+
+        <h2>Plantio</h2>
+
+        <p>
+        O morango gosta de solo úmido
+        e bastante luz solar.
+        </p>
+
+    `;
+}
